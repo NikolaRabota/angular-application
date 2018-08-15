e729066ee07a7e86d216d3509c7ef7e6362999ad
NikolaRabota <42212874+NikolaRabota@users.noreply.github.com>
2018-08-15 16:53:11 +0200

    Update user-detail.component.ts

--- src/app/dashboard/components/user-detail/user-detail.component.ts
+++ src/app/dashboard/components/user-detail/user-detail.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, ChangeDetectionStrategy} from '@angular/core';
 import { User} from "../../../user";
+import {UserService} from "../../services/user.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
+  changeDetection: ChangeDetectionStrategy.Default,
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
 
-  @Input() user: User
-  constructor() { }
+  public user$: User;
 
-  ngOnInit() {
+  constructor(private userService: UserService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.getUser();
   }
 
+  getUser(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getUser(id).subscribe(user => {
+        this.user$ = user;
+      }
+    );
+  }
 }
