bcba2fed96a64dd3d68e81c3ba3377ccf32b2f17
NikolaRabota <42212874+NikolaRabota@users.noreply.github.com>
2018-08-15 16:50:49 +0200

    Update post.service.ts

--- src/app/dashboard/services/post.service.ts
+++ src/app/dashboard/services/post.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable} from "rxjs";
+import {Observable} from "rxjs";
 import {Post} from "../../post";
 import {HttpClient} from "@angular/common/http";
 
@@ -13,6 +13,10 @@ export class PostService {
   getPosts(): Observable<Post[]> {
     // @ts-ignore
     return this.http.get('https://jsonplaceholder.typicode.com/posts')
+  }
+  getPostFromService(id: number): Observable<Post> {
+    // @ts-ignore
+    return this.http.get("https://jsonplaceholder.typicode.com/posts/".concat(id.toString()))
   }
 }
 
